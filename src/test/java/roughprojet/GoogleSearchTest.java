41d81a0f39be421932a612107c79ca3d8bd05480
akash <nickjain.1304@gmail.com>
2020-06-28 14:22:44 -0400

    mavenrough firstupload

---
+++ src/test/java/roughprojet/GoogleSearchTest.java
@@ -0,0 +1,12 @@
+package roughprojet;
+
+import org.testng.annotations.Test;
+
+public class GoogleSearchTest {
+	
+	@Test
+	public void gsearch() {
+		System.out.println("inside gsearch");
+	}
+
+}
---
+++ src/test/java/roughprojet/Guru99Test.java
@@ -0,0 +1,22 @@
+package roughprojet;
+
+import org.testng.annotations.Test;
+
+public class Guru99Test {
+
+	
+	
+	@Test
+	public void login()
+	{
+		System.out.println("inside login");
+		
+	}
+	
+	@Test
+	public void newcustomer()
+	{
+		System.out.println("inside newcustomer");
+	}
+}
+
